d845d9b07c4dc93b21f5fa9e083433699d8e1043
Jay <jayeshpatil8041@gmail.com>
2024-08-02 16:24:16 +0530

    create symlinks for env file

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :branch, "main"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, '.env' #"config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
